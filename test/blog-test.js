106c07f9bb4ac6fdbd73557c5621d7e775b95ee2
kito0 <drewshah0@gmail.com>
2022-05-13 18:59:57 -0600

    test: add blog-test.js

---
+++ test/blog-test.js
@@ -0,0 +1,39 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('Blog', function () {
+	it('Should create a post', async () => {
+		const Blog = await ethers.getContractFactory('Blog');
+		const blog = await Blog.deploy('Test blog');
+		await blog.deployed();
+		await blog.createPost('Test post', 'Test content');
+
+		const posts = await blog.fetchPosts();
+		expect(posts[0].title).to.equal('Test post');
+	});
+
+	it('Should edit a post', async () => {
+		const Blog = await ethers.getContractFactory('Blog');
+		const blog = await Blog.deploy('Test blog');
+		await blog.deployed();
+
+		await blog.createPost('Test post', 'Test content');
+		await blog.updatePost(1, 'Updated post', 'Updated content', true);
+
+		posts = await blog.fetchPosts();
+		expect(posts[0].title).to.equal('Updated post');
+	});
+
+	it('Should update the name', async () => {
+		const Blog = await ethers.getContractFactory('Blog');
+		const blog = await Blog.deploy('Test blog');
+		await blog.deployed();
+
+		let name = await blog.name();
+		expect(name).to.equal('Test blog');
+
+		await blog.updateName('Updated blog name');
+		name = await blog.name();
+		expect(name).to.equal('Updated blog name');
+	});
+});
