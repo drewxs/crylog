76748256466dfbfce0c8747e7254804b218558fd
kito0 <drewshah0@gmail.com>
2022-05-15 22:56:11 -0600

    chore: add husky pre-commit hook and linting configurations

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-	reactStrictMode: false,
-	images: {
-		domains: ['ipfs.io'],
-	},
+  reactStrictMode: false,
+  images: {
+    domains: ['ipfs.io'],
+  },
+  eslint: {
+    dirs: ['pages', 'components', 'scripts', 'styles', 'test', 'contracts'],
+  },
 };
