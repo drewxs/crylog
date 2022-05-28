8935977b579e57238257b81177906bfe490ceb74
kito0 <drewshah0@gmail.com>
2022-05-28 08:30:35 -0600

    build(next): add buildtime env

--- next.config.js
+++ next.config.js
@@ -18,4 +18,9 @@ module.exports = {
       'utils',
     ],
   },
+  env: {
+    ENVIRONMENT: process.env.ENVIRONMENT,
+    NEXT_PUBLIC_ENVIRONMENT: process.env.NEXT_PUBLIC_ENVIRONMENT,
+    ACCOUNT_PRIVATE_KEY: process.env.ACCOUNT_PRIVATE_KEY,
+  },
 };
