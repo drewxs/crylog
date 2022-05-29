939b4ce1c0f7143c8a47f70ae5754cd860d21fe3
kito0 <drewshah0@gmail.com>
2022-05-29 02:09:30 -0600

    build(hardhat): update testnet url

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,13 +11,13 @@ const config: HardhatUserConfig = {
       chainId: 1337,
     },
     mumbai: {
-      url: 'https://rpc-mumbai.matic.today',
-      accounts: [process.env.ACCOUNT_PRIVATE_KEY ?? ''],
+      url: 'https://matic-mumbai.chainstacklabs.com',
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
     },
-    // polygon: {
-    //   url: 'https://polygon-rpc.com',
-    //   accounts: [process.env.ACCOUNT_PRIVATE_KEY],
-    // },
+    polygon: {
+      url: 'https://polygon-rpc.com',
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+    },
   },
 };
 
